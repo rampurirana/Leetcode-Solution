93dd6a270bc985fd4bf88ab600e1461a1acdb32e
Rampravesh Rana <rampraveshrana1435@gmail.com>
2026-02-02 17:39:35 +0530

    Update

--- Leetcode Problems/src/TwoSum.java
+++ Leetcode Problems/src/TwoSum.java
@@ -2,18 +2,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
-    public static void main (String[] args){
-        int[] nums = {2, 7, 11, 15};
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
         System.out.println(Arrays.toString(twoSum(nums, 9)));
     }
 
-    public static int[] twoSum(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if(map.containsKey(complement)){
-                return new int[] {map.get(complement), i};
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
             map.put(nums[i], i);
         }
