28748d9d0f8994c6157b932226b6055827d14840
Rampravesh Rana <rampraveshrana1435@gmail.com>
2026-02-03 17:14:21 +0530

    Roman To Integer Solution

---
+++ Leetcode Problems/src/RomanToInteger.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static void main(String[] args){
+        String s = "MCMXCIV";
+        System.out.println(RomToInt(s));
+    }
+
+    public static int RomToInt(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for(int i = 0; i < s.length() - 1; i++){
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                result = result - map.get(s.charAt(i));
+            } else {
+                result = result + map.get(s.charAt(i));
+            }
+        }
+
+        result = result + map.get(s.charAt(s.length()-1));
+
+        return result;
+    }
+}
